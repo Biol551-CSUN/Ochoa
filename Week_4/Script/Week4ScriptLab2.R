5e5e1fc2a62ea062da0c60260509e8b669d5f936
Ashlee Ochoa <ashleeochoa@Ashlees-MacBook-Air.local>
2022-02-19 15:57:50 -0800

    commit

--- Week_4/Script/Week4ScriptLab2.R
+++ Week_4/Script/Week4ScriptLab2.R
@@ -5,6 +5,8 @@
 ### load libraries 
 library(tidyverse)
 library(here)
+library(viridis)
+library(extrafont)
 
 ### Load Data 
 ChemData <- read_csv(here("Week_4", "Data", "chemicaldata_maunalua.csv"))
@@ -42,11 +44,15 @@ write_csv(here("Week_4", "Output", "LABSTATS.csv"))
 View(ChemData1)
 
 ChemData2 %>% 
-  ggplot(aes(x= Variables, y = log10(Param_means +1) ,color=Time)) +
+  ggplot(aes(x= Variables, y = log10(Param_means +1),color=Time)) +
   geom_point()+ 
-  scale_y_discrete("Variables")
-  labs(x = "Variables", y= "Log of Param means")+ 
-  theme(axis.title = element_text(Temp_in = "Temp in")) 
+  ggtitle("Hawai'i Biogeochemistry Data") +
+  theme(plot.title= element_text(hjust = 0.5))+
+  labs(x = "Variables", y= "Log of mean parameter") +
+  theme_light()+
+  choose_font(c("sans"), quiet = TRUE)
+
   
 View(ChemData2)
 
+ggsave(here("Week_4", "Output", "Biogeochemgraph.png"))
